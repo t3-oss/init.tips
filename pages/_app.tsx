a032e37fbb01f28cb03f1947474050cd5864ad13
theo <me@t3.gg>
2021-09-01 23:12:06 -0700

    Plausible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <script
+          defer
+          data-domain="init.tips"
+          src="https://plausible.io/js/plausible.js"
+        ></script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
-export default MyApp
+export default MyApp;
