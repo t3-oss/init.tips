e61c1d3206a3d8d4d9506636addf1e0557a99d5c
Mark R. Florkowski <mark.florkowski@gmail.com>
2021-09-01 21:26:49 -0700

    decription width, add placeholder text

--- pages/index.tsx
+++ pages/index.tsx
@@ -55,9 +55,26 @@ const RecommendationPage = () => {
 const WhyPage = () => {
   return (
     <div className="flex flex-col items-center h-screen justify-center cursor-default relative bg-gray-400 text-gray-700">
-      <h2 id="why" className="text-lg">
-        Why Next.js and Typescript?
-      </h2>
+      <div className="max-w-md md:max-w-2xl">
+        <h2 id="why" className="text-lg font-bold">
+          Why Next.js and Typescript?
+        </h2>
+        <p>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
+          ab illo inventore veritatis et quasi architecto beatae vitae dicta
+          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
+          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
+          qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
+          dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
+          quia non numquam eius modi tempora incidunt ut labore et dolore magnam
+          aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
+          exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex
+          ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
+          ea voluptate velit esse quam nihil molestiae consequatur, vel illum
+          qui dolorem eum fugiat quo voluptas nulla pariatur
+        </p>
+      </div>
     </div>
   );
 };
