4c4bca5308a3a0229ba1f1432315ea9591721d16
theo <me@t3.gg>
2021-09-03 17:27:43 -0700

    Outbound too

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,7 +4,7 @@ import PlausibleProvider from "next-plausible";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <PlausibleProvider domain="init.tips">
+    <PlausibleProvider domain="init.tips" trackOutboundLinks>
       <Component {...pageProps} />
     </PlausibleProvider>
   );
