015561cc7de86722debae3543f836c26fd9cfe1e
theo <me@t3.gg>
2021-09-03 17:18:49 -0700

    Manual rewrites out of desperation

--- next.config.js
+++ next.config.js
@@ -5,4 +5,17 @@ module.exports = withPlausibleProxy()({
     locales: ["en-US"],
     defaultLocale: "en-US",
   },
+  target: "serverless",
+  async rewrites() {
+    return [
+      {
+        source: "/js/script.js",
+        destination: "https://plausible.io/js/plausible.js",
+      },
+      {
+        source: "/api/event",
+        destination: "https://plausible.io/api/event",
+      },
+    ];
+  },
 });
