bff5375ca74f785971ac7717f37f590972e8fb39
theo <me@t3.gg>
2021-09-04 19:55:41 -0700

    Safari is dumb

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -52,7 +52,7 @@ export const CustomSiteHead: React.FC<{
         href="/favicon-16x16.png"
       />
       <link rel="manifest" href="/site.webmanifest" />
-      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#9ca3af" />
       <meta name="msapplication-TileColor" content="#da532c" />
       <meta name="theme-color" content="#ffffff" />
     </Head>
