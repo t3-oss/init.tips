569685853ccacc1c210ee61e6a41879ef675b0db
theo <me@t3.gg>
2021-09-19 22:08:03 -0700

    Restore for now

--- next.config.js
+++ next.config.js
@@ -6,4 +6,18 @@ module.exports = withPlausibleProxy()({
     defaultLocale: "en-US",
   },
   target: "serverless",
+  async rewrites() {
+    return [
+      {
+        source: "/js/script.js",
+        destination: "https://plausible.io/js/plausible.js",
+        locale: false,
+      },
+      {
+        source: "/api/event",
+        destination: "https://plausible.io/api/event",
+        locale: false,
+      },
+    ];
+  },
 });
