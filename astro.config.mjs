58f2266af6247e4b78140f4e6b64a8e07b815f7f
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2022-07-08 00:07:33 +0200

    fix: use std tailwind instead of the integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
-
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [],
 });
